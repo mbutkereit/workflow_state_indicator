f7345b0b822d7f6a512af67e847378aa440b667d
Marvin Butkereit <marvin.butkereit@comm-press.de>
2019-10-10 08:26:38 +0200

    Second init

---
+++ src/WorkflowStateIndicatorInformation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\workflow_state_indicator;
+
+/**
+ * Class WorkflowStateIndicatorInformation.
+ *
+ * @package Drupal\workflow_state_indicator
+ */
+class WorkflowStateIndicatorInformation {
+
+}
